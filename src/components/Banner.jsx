26966270cd27a95640d6d3074e9fa0b90e48196c
Steven Huang <steven.charles.huang@gmail.com>
2018-07-28 00:04:55 -0400

    fixed header styling

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,9 +9,11 @@ export default function () {
     <div className="Banner">
       <h1>Steven Charles Huang</h1>
       <h2>[ Web Developer ]</h2>
-      <a href="https://github.com/stevencharleshuang/" target="_blank"><img src={Git} alt="Git Hub Link" /></a>
-      <a href="https://www.linkedin.com/in/stevencharleshuang/" target="_blank"><img src= {LinkedIn} alt="Linked In Link" /></a>
-      <a href={ResumePDF} target="_blank"><img src={Resume} alt="Resume Link" /></a>
+      <div className="MyStuff">
+        <a href="https://github.com/stevencharleshuang/" target="_blank"><img src={Git} alt="Git Hub Link" /></a>
+        <a href="https://www.linkedin.com/in/stevencharleshuang/" target="_blank"><img src= {LinkedIn} alt="Linked In Link" /></a>
+        <a href={ResumePDF} target="_blank"><img src={Resume} alt="Resume Link" /></a>
+      </div>
     </div>
   )
 }
