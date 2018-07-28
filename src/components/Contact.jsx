85635594c0d00aead6ab52d59e3d9bbc4f235cdf
Steven Huang <steven.charles.huang@gmail.com>
2018-07-28 02:21:27 -0400

    reformnatted About Component, added Contact Component with Netlify sample code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,31 @@ import React from 'react';
 
 export default function () {
   return (
-    <h1>You reached the Contact page, Dave</h1>
-  )
+    <div className="Contact">
+      <section>
+        E-mail me at <a href="mailto:steven.charles.huang@gmail.com">steven.charles.huang@gmail.com</a>
+      </section>
+      <p>or</p>
+        Leave a note
+      <section class="contact-form">
+        <form name="contact" method="POST" netlify>
+          <p>
+            <label>Name: <br />
+            <input type="text" name="name" /></label>
+          </p>
+          <p>
+            <label>Email: <br />
+            <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Message: <br />
+            <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </section>
+    </div>
+  );
 }
