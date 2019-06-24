38b87c9ae31b46a39c2ad91eb91ddb24fa69ee52
Steven Huang <steven.charles.huang@gmail.com>
2019-06-24 14:07:08 -0400

    refactored banner, updated styles

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import MyStuff from './MyStuff';
 
 export default function (props) {
   return (
     <div className="Banner">
-      <h1>Steven Charles Huang</h1>
+      <Link to="/">
+        <h1>Steven Charles Huang</h1>
+      </Link>
       <h2>[ Web Developer ]</h2>
       <MyStuff />
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,6 @@ export default function () {
   return (
     <nav>
       <ul>
-        <Link to="/"><li>Home</li></Link>
         <Link to="/about"><li>About</li></Link>
         <Link to="/projects"><li>Projects</li></Link>
         <Link to="/contact"><li>Contact</li></Link>
